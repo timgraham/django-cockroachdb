d24bb37cd20c18b05aa0e3127fe44100b7ca66b1
Tim Graham <timograham@gmail.com>
2023-06-21 16:48:19 -0400

    move CI to github actions

--- teamcity-build/build-teamcity-22.2-psycopg2.sh
+++ teamcity-build/build-teamcity-22.2-psycopg2.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-export USE_PSYCOPG2=1
-./teamcity-build/build-teamcity.sh "v22.2.7"
+# Obsolete
--- teamcity-build/build-teamcity.sh
+++ teamcity-build/build-teamcity.sh
@@ -1,44 +1,23 @@
 #!/usr/bin/env bash
 set -x
 
-# this script is meant to be invoked by one of the build-teamcity-$VERSION.sh scripts
-
-VERSION="${1:-v19.2.5}"
-
 # clone django into the repo.
 rm -rf _django_repo
 git clone --depth 1 --single-branch --branch cockroach-4.2.x https://github.com/timgraham/django _django_repo
 
 # install the django requirements.
 cd _django_repo/tests/
-pip3 install -e ..
-pip3 install -r requirements/py3.txt
+pip install --user -e ..
+pip install --user -r requirements/py3.txt
 if [[ -z "${USE_PSYCOPG2}" ]]; then
-    pip3 install -r requirements/postgres.txt
+    pip install --user -r requirements/postgres.txt
 else
-    pip3 install psycopg2
+    pip install --user psycopg2
 fi
 cd ../..
 
 # install the django-cockroachdb backend.
-pip3 install .
-
-# download and start cockroach
-if [ $VERSION == "LATEST" ]
-then
-    SPATIAL_LIBS="--spatial-libs=$PWD"
-    wget "https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST" -O cockroach_exec
-    wget "https://edge-binaries.cockroachdb.com/cockroach/lib/libgeos.linux-gnu-amd64.so.LATEST" -O libgeos.so
-    wget "https://edge-binaries.cockroachdb.com/cockroach/lib/libgeos_c.linux-gnu-amd64.so.LATEST" -O libgeos_c.so
-    chmod +x cockroach_exec
-else
-    SPATIAL_LIBS="--spatial-libs=cockroach-${VERSION}.linux-amd64/lib"
-    wget "https://binaries.cockroachdb.com/cockroach-${VERSION}.linux-amd64.tgz"
-    tar -xvf cockroach-${VERSION}*
-    cp cockroach-${VERSION}*/cockroach cockroach_exec
-fi
-
-./cockroach_exec start-single-node --insecure $SPATIAL_LIBS &
+pip install --user .
 
 cd _django_repo/tests/
 
@@ -47,4 +26,4 @@ cp ../../teamcity-build/cockroach_settings.py .
 cp ../../teamcity-build/cockroach_gis_settings.py .
 
 # Run the tests!
-python3 ../../teamcity-build/runtests.py
+python3 ../../teamcity-build/runtests.py basic
