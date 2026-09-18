c6010db76aa409c412a3effd6112c84993dce03f
Tim Graham <timograham@gmail.com>
2026-09-18 09:25:01 -0400

    Run tests with --parallel=1

--- django-test-suite/runtests.py
+++ django-test-suite/runtests.py
@@ -4,11 +4,11 @@ import sys
 # This file assumes it is being run in the django/tests/ repo.
 
 shouldFail = False
-res = os.system("python3 runtests.py --settings cockroach_settings -v 2")
+res = os.system("python3 runtests.py --settings cockroach_settings -v 2 --parallel=1")
 if res != 0:
     shouldFail = True
 
-res = os.system("python3 runtests.py gis_tests --settings cockroach_gis_settings -v 2")
+res = os.system("python3 runtests.py gis_tests --settings cockroach_gis_settings -v 2 --parallel=1")
 if res != 0:
     shouldFail = True
 
